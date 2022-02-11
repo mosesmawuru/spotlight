24cf6ebb4da12f01a263e31c2f613950760d23ba
gladiator0428 <lovely4you2ai8ni@gmail.com>
2022-02-12 04:15:37 +0800

    feat: complete all

--- src/layouts/landing/glimpse/glimpse.styled.tsx
+++ src/layouts/landing/glimpse/glimpse.styled.tsx
@@ -4,6 +4,15 @@ export const GlimpseSection = styled.div`
   background-color: #ffffff;
   border-radius: 30px;
   padding: 65px 115px 50px;
+  @media screen and (max-width: 1024px) {
+    padding: 54px 40px;
+  }
+  @media screen and (max-width: 600px) {
+    padding: 43px 20px;
+  }
+  @media screen and (max-width: 320px) {
+    padding: 43px 5px;
+  }
 `;
 
 export const GlimpseTitle = styled.div`
@@ -11,4 +20,9 @@ export const GlimpseTitle = styled.div`
   font-size: clamp(22px, 4.3vw, 35px);
   color: #051730;
   text-align: center;
+`;
+
+export const GlimpsSlider = styled.div`
+  margin-top: 48px;
+  width: 100%;
 `;
--- src/layouts/landing/glimpse/index.tsx
+++ src/layouts/landing/glimpse/index.tsx
@@ -1,10 +1,54 @@
-import React from "react";
-import { GlimpseSection, GlimpseTitle } from "./glimpse.styled";
+import React, { useEffect, useState } from "react";
+import { GlimpseSection, GlimpseTitle, GlimpsSlider } from "./glimpse.styled";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Pagination, Navigation } from "swiper";
+
+import slider1 from "assets/image/slider1.png";
+import slider2 from "assets/image/slider2.png";
+import slider3 from "assets/image/slider3.png";
 
 const GlimpsePart = () => {
+  const [screenW, setScreenW] = useState(0);
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.addEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleResize = () => {
+    setScreenW(window.innerWidth);
+  };
+
   return (
     <GlimpseSection>
       <GlimpseTitle>Get a glimpse</GlimpseTitle>
+      <GlimpsSlider>
+        <Swiper
+          slidesPerView={screenW > 1024 ? 3 : 1}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={screenW > 1024 ? false : true}
+          modules={screenW < 1024 ? [Pagination, Navigation] : []}
+          className="mySwiper"
+          loop={true}
+        >
+          <SwiperSlide>
+            <img src={slider1} alt="slider1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider2} alt="slider1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider3} alt="slider1" />
+          </SwiperSlide>
+        </Swiper>
+      </GlimpsSlider>
     </GlimpseSection>
   );
 };
